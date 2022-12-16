820c5de82eafac7e29d3645353fe191f1af4850d
devCecy <im.ceciliaan@gmail.com>
2022-12-16 16:45:14 +0900

    feat : next-seo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { NextSeo } from "next-seo";
 import type { NextPage } from "next";
 import styled from "@emotion/styled";
 import Head from "next/head";
@@ -6,19 +7,19 @@ import { getAllPosts } from "../lib/api";
 
 // components
 import PostItem from "../components/Post/PostItem";
+import { useRouter } from "next/router";
 
 const Home: NextPage = ({ allPosts }: any) => {
+	const router = useRouter();
 	return (
 		<>
-			<Head>
-				<title>DEVCECY LOG</title>
-				<meta name="description" content="devCecy의 블로그입니다 :-)" />
-				{/* <link rel="icon" href="/favicon.ico" /> */}
-			</Head>
-
+			<NextSeo
+				title="DEVCECY LOG"
+				description="안녕하세요, 프론트엔드 개발자 안미현입니다. 저의 기록이 개발 생태계에 조금이나마 도움이 되었으면 합니다."
+			/>
 			{/* 포스트 리스트 */}
 			{allPosts.map((post: any, idx: string) => (
-				<PostList key={idx} href={`/posts/${post.slug}`}>
+				<PostList key={idx} onClick={() => router.push(`/posts/${post.slug}`)}>
 					<PostItem post={post} />
 				</PostList>
 			))}
@@ -42,14 +43,16 @@ export const getStaticProps = async () => {
 		props: { allPosts },
 	};
 };
-const PostList = styled(Link)`
+const PostList = styled.div`
 	width: 100%;
 	margin-bottom: 30px;
 	padding: 20px;
+
 	&:hover {
 		transition: 0.1s ease-in-out;
 		background-color: #fafafa;
 		border-radius: 10px;
+		cursor: pointer;
 	}
 
 	@media screen and (max-width: 480px) {
