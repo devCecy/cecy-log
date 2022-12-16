c374e876f7253f72620edc327374dd0f4164db64
devCecy <im.ceciliaan@gmail.com>
2022-12-16 21:42:02 +0900

    fix : com->me

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-	siteUrl: "https://devcecy.com",
+	siteUrl: "https://devcecy.me",
 	changefreq: "daily",
 	generateRobotsTxt: true,
 	robotsTxtOptions: {
