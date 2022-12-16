38d416cd0eed73b1f8c4e074db9814f3da2712ca
devCecy <im.ceciliaan@gmail.com>
2022-12-16 15:49:24 +0900

    Create : theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,21 @@
-import "../../styles/globals.css";
 import type { AppProps } from "next/app";
+
+// emotion theme
+import GlobalStyle from "../../styles/globals";
+import theme from "../../styles/theme";
+import { ThemeProvider } from "@emotion/react";
 
 // components
 import Layout from "../components/Layout/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
+		<ThemeProvider theme={theme}>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+			<GlobalStyle />
+		</ThemeProvider>
 	);
 }
 
---
+++ styles/theme.ts
@@ -0,0 +1,9 @@
+const colors = {
+	main: "#FAAB78",
+};
+
+const theme = {
+	colors,
+};
+
+export default theme;
